901a844bd1f0d802434b36cc69beffd9e33f43dd
SolfugaSoft <SolfugaSoft@gmail.com>
2018-08-09 16:25:41 +0200

    First change

--- JavaGitRepository/src/Main.java
+++ JavaGitRepository/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello GIT");
+System.out.println("And now i can pushing code to remote repository");
 	}
 
 }
