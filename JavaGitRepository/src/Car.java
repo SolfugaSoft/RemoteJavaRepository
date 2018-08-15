2a71f9625591d9940958a9547cb0fdf3cafb8ece
SolfugaSoft <SolfugaSoft@gmail.com>
2018-08-15 13:05:16 +0200

    Add some Objects

---
+++ JavaGitRepository/src/Car.java
@@ -0,0 +1,40 @@
+public class Car {
+	public static void main(String[] args) {
+
+		SportCar Arrinera = new SportCar();
+		Arrinera.color = "red";
+		Arrinera.doors = 2;
+		Arrinera.name = "Arrinera";
+		Arrinera.wheels = 4;
+		
+		SportCar Spyker = new SportCar();
+		Spyker.color = "red";
+		Spyker.doors = 2;
+		Spyker.name = "Arrinera";
+		Spyker.wheels = 4;
+		
+		Arrinera.run();
+		
+	
+		
+
+		System.out.println(Arrinera.name);
+		System.out.println(Spyker.doors);
+		
+		
+
+	}
+}
+class SportCar{
+	String color;
+	String name;
+	int doors;
+	int wheels;
+	
+	void run(){
+	}
+	}
+		
+	
+
+
