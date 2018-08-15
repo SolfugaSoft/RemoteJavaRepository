22104569dd2929295b2b896c8409a4c2f8522de4
SolfugaSoft <SolfugaSoft@gmail.com>
2018-08-15 13:05:40 +0200

    object without wacek

--- JavaGitRepository/src/Car.java
+++ JavaGitRepository/src/Car.java
@@ -6,35 +6,27 @@ public class Car {
 		Arrinera.doors = 2;
 		Arrinera.name = "Arrinera";
 		Arrinera.wheels = 4;
-		
+
 		SportCar Spyker = new SportCar();
 		Spyker.color = "red";
 		Spyker.doors = 2;
 		Spyker.name = "Arrinera";
 		Spyker.wheels = 4;
-		
+
 		Arrinera.run();
-		
-	
-		
 
 		System.out.println(Arrinera.name);
 		System.out.println(Spyker.doors);
-		
-		
 
 	}
 }
-class SportCar{
+
+class SportCar {
 	String color;
 	String name;
 	int doors;
 	int wheels;
-	
-	void run(){
-	}
-	}
-		
-	
 
-
+	void run() {
+	}
+}
